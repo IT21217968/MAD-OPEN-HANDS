cd4d5a0415304fbe3fbd98626580cd931d62eb37
Sethmi <damyadharmawardhana@gmail.com>
2023-05-04 20:02:31 +0530

    First commit

---
+++ app/src/main/java/com/example/project_management/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.project_management
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        val appBarConfig = AppBarConfiguration(setOf(R.id.home_fragment,R.id.goal_fragment,R.id.notification_fragment,R.id.profile_fragment))
+        setupActionBarWithNavController(navController,appBarConfig)
+        bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
